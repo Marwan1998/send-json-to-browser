13cd6aeb81bcadd0b8fdf4db05d5c00bd145590c
Marwan Algadi <marwan.algadi1998@gmail.com>
2021-02-23 23:09:03 +0200

    project completed with the functionility

--- app.js
+++ app.js
@@ -10,15 +10,16 @@ app.use(bodyParser.urlencoded({
 app.use(express.static("public"));
 
 
-
 app.route("/")
-.get(function(req, res){
-  res.sendFile(__dirname + '/main.html');
-})
-.post(function(req, res){
-  console.log(req.body);
-});
+  .get(function(req, res) {
+    res.sendFile(__dirname + '/main.html');
+  })
 
+  .post(function(req, res) {
+    const json = req.body.json;
+    // res.redirect("/");   //it's not right to do it.
+    res.send(json);
+  });
 
 
 app.listen(3000, function() {
